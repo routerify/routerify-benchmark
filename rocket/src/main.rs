3f4270d4ac5798d09330d02dc973dfd465bc00ea
Sergey Glushchenko <sergey@fiberplane.com>
2021-03-03 08:39:56 +0100

    Update versions and run benchmarks again

--- rocket/src/main.rs
+++ rocket/src/main.rs
@@ -1,13 +1,12 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use]
 extern crate rocket;
 
 #[get("/")]
-fn hello() -> String {
-    format!("Hello, World!")
+fn hello() -> &'static str {
+    "Hello, world!"
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+#[launch]
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![hello])
 }
